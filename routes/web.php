286f35204f40e0efde5b9dd3383cfb982d28f393
Dung Nguyen <nqdd251003@gmail.com>
2024-08-30 20:16:53 +0700

    [Feature] Delete file don't use

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Admin\StudentController;
 
 Route::middleware('web')->prefix('api/v1/admin/students')->group(function () {
-    Route::put('/{id}', [StudentController::class, 'update']);
-    Route::post('', [StudentController::class, 'store']);
     Route::get('', [StudentController::class, 'index']);
-    // Route::get('/{id}', [StudentController::class, 'show']);
+    Route::get('/{id}', [StudentController::class, 'show']);
+    Route::post('', [StudentController::class, 'store']);
+    Route::put('/{id}', [StudentController::class, 'update']);
     Route::delete('/{id}', [StudentController::class, 'destroy']);
 });
